f8664e552fce5da6f5a88b8dbf0077ae4fca7a88
Slava <chugunov-viach@mail.ru>
2023-01-28 18:50:34 +0300

    fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import config as cf
 from aiogram import Bot, Dispatcher, executor, types
 ribov_list = [1,2,3,4,5,6,7,8,]
+ribov_dict = {"селедка":4, "семга": 56, "Яз": 10}
 token = cf.token
 tgbot = Bot(token)
 dp = Dispatcher(tgbot)
@@ -13,6 +14,12 @@ help_message = """
 <b>/print</b> - <em>елочка</em>
 <b>/buy</b> - <em>покупка рыб</em>
 """
+def print_dict (ribov):
+    tmp = ""
+    for i in ribov:
+        tmp += f'{i} : {ribov[i]} \n'
+        return tmp
+
 @dp.message_handler(commands =['help'])
 async def send_help_command(message: types.Message):
  await tgbot.send_message(message.chat.id, help_message, "HTML")
@@ -27,24 +34,30 @@ async def send_welcom(massege : types.Message):
 
 @dp.message_handler(commands=['buy'])
 async def send_buy_command(massege: types.Message):
+    print_dict(ribov_dict)
     args = massege.get_args()
-    if len(ribov_list) == 0:
-        return await tgbot.send_message(massege.chat.id,
-                                        "братан прости риба протухла вся, приходи завтра")
+    args = args.split()
+    print(ribov_dict[args[0]])
+    print(args[1])
+
     if not args:
         return await tgbot.send_message(massege.chat.id,
-                                         f" братан есть сейчас только {len(ribov_list)} рибов"
+                                         f" братан есть сейчас только \n{print_dict(ribov_dict)} рибов"
                                          f"Сколько рибов вы хотите")
     else:
-        if args.isdigit():
-            args = int(args)
-            if args > len(ribov_list):
+        if args[0] in ribov_dict:
+            return await tgbot.send_message(massege.chat.id, f"сорян такой рыбы нет")
+        if ribov_dict[args[0]] == 0:
+            return await tgbot.send_message(massege.chat.id,
+                                            "братан прости риба протухла вся, приходи завтра")
+        if args[1].isdigit():
+            args[1] = int(args[1])
+            if args[1] > ribov_dict[args[0]]:
                 return await tgbot.send_message(massege.chat.id,
                                                 "слишком много риб хочешь, нет столько")
             else:
-                for i in range(args):
-                    ribov_list.pop()
-                return await tgbot.send_message(massege.chat.id, f"смари, есть еще {len(ribov_list)} рибов")
+                ribov_dict[args[0]] = ribov_dict[args[0]] - args[1]
+                return await tgbot.send_message(massege.chat.id, f"смари, есть еще \n{print_dict(ribov_dict)} рибов")
         else:
             return await tgbot.send_message(massege.chat.id,"еще раз скажи сколько рибов")
 
